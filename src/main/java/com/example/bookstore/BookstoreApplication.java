961ff1298ab7955eb93daf95170bb80bd87a121b
Lauri Juusti <lauri.juusti@myy.haaga-helia.net>
2024-09-15 20:34:25 +0300

    added JPA and H2 support, added test data, enabled H2 console

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -2,6 +2,11 @@ package com.example.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.bookstore.BookRepository;
+import com.example.bookstore.domain.Book;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,15 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+@Bean
+    CommandLineRunner initData(BookRepository bookRepository) {
+        return args -> {
+            bookRepository.save(new Book("Tuntematon Sotilas", "Väinö Linna", 1954, 987654, 14.99f));
+			bookRepository.save(new Book("Sinuhe Egyptialainen", "Mika Waltari", 1945, 876543, 19.99f));
+			bookRepository.save(new Book("Puhdistus", "Sofi Oksanen", 2008, 765432, 12.99f));
+
+            
+            System.out.println("Test data added successfully");
+        };
+	}
 }
--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -1,7 +1,17 @@
 package com.example.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
     public String title;
     public String author;
     public int publicationYear;
